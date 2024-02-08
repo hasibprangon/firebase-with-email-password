0589fae66675fe132e6d8f8993c3a6c3503064e9
hasibprangon <hasibulhossainp09@gmail.com>
2024-02-08 21:37:43 +0600

    get email from input section

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const handleEmailChange = (event) => {
+        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
     return (
         <div>
             <h2 className='text-3xl font-semibold mb-5'>Please Register here</h2>
             <form>
-                <input className='border rounded-md px-5 mb-3 border-black py-2' type="email" name="email" id="" placeholder='Enter your Email Here' />
+                <input onChange={handleEmailChange} className='border rounded-md px-5 mb-3 border-black py-2' type="email" name="email" id="" placeholder='Enter your Email Here' />
                 <br />
                 <input className='border rounded-md px-5 mb-3 border-black py-2' type="password" name="password" id="" placeholder='Enter your Email Here' />
                 <br />
-                <input className=' bg-indigo-400 hover:bg-indigo-600 p-2 text-white border rounded-r-md' type="submit" value="Register" />
+                <input className=' bg-indigo-400 hover:bg-indigo-600 p-2 text-white border rounded-md' type="submit" value="Register" />
             </form>
         </div>
     );
